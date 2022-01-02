f3db96ded5c4718148c06b710ef0b2c375dbc7e6
emilienGallet <emilienflorentclaude.gallet@gmail.com>
2022-01-02 23:14:45 +0100

    Passage en DEV

--- DirtyBiologistan/src/main/java/org/dirtybiologistan/DeployInit.java
+++ DirtyBiologistan/src/main/java/org/dirtybiologistan/DeployInit.java
@@ -2,7 +2,7 @@ package org.dirtybiologistan;
 
 public class DeployInit {
 	
-	public final static Boolean isLive=true;
+	public final static Boolean isLive=false;
 
 	
 	///////////////////////////////////////////////////////
@@ -15,19 +15,19 @@ public class DeployInit {
 	///////////////////////////////////////////////////////
 	
 	//Debug mod 
-	/*
+	/**/
 	public static final String Default_Path = System.getProperty("user.dir");
 	public static final String PathDeploy = Default_Path+"/src/main/resources";//+"/ROOT/WEB-INF/classes";
 	public static final String PathImgDeploy = PathDeploy + "/static";//""
 	public static final String PathResourcesDeploy = PathDeploy;
 	public static final String NomDeDomaine = "http://localhost:8080/";
-	*/
-	//Live Mod
 	/**/
+	//Live Mod
+	/*
 	public static final String Default_Path = "";
 	public static final String PathDeploy = Default_Path+"";
 	public static final String PathImgDeploy = PathDeploy + "";//""
 	public static final String PathResourcesDeploy = "/dirtybiologistan";
 	public static final String NomDeDomaine = "https://www.dirtybiologistan.org/";//Déja pris a modifier
-	/**/
+	*/
 }
--- DirtyBiologistan/src/main/java/org/dirtybiologistan/DirtyBiologistanApplication.java
+++ DirtyBiologistan/src/main/java/org/dirtybiologistan/DirtyBiologistanApplication.java
@@ -37,9 +37,9 @@ public class DirtyBiologistanApplication {
     private Connector redirectConnector() {
         Connector connector = new Connector("org.apache.coyote.http11.Http11NioProtocol");
         connector.setScheme("http");
-        connector.setPort(80);
+        connector.setPort(8080);
         connector.setSecure(false);
-        connector.setRedirectPort(443);
+        connector.setRedirectPort(8443);
         return connector;
     }
 }
